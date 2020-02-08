7bb735f3aa2d28302134c62a25a242fa57a1fb39
jmickle66666666 <jaymickle@gmail.com>
2020-02-08 14:23:40 +0100

    !!

--- Assets/Nodes/BrushGenerator.cs
+++ Assets/Nodes/BrushGenerator.cs
@@ -5,7 +5,7 @@ using Chisel.Components;
 using Chisel.Core;
 
 [ExecuteInEditMode]
-public class BrushGenerator : ChiselGeneratorComponent
+public class BrushGenerator : ChiselGeneratorComponent, ISerializationCallbackReceiver
 {
     public GeneratorGraph generator;
     public BrushMesh[] brushMeshes;
@@ -14,7 +14,7 @@ public class BrushGenerator : ChiselGeneratorComponent
     public const string kNodeTypeName = "NodeGenerator";
     public override string NodeTypeName { get { return kNodeTypeName; } }
 
-    public Dictionary<string, object> properties = new Dictionary<string, object>();
+    // public Dictionary<string, object> properties = new Dictionary<string, object>();
     GeneratorGraph graphInstance;
 
     protected override void UpdateGeneratorInternal()   {
@@ -41,5 +41,30 @@ public class BrushGenerator : ChiselGeneratorComponent
     {
         generator.RefreshInputNodes();
         base.UpdateGenerator();
+    }
+
+    public Dictionary<string, Bounds> properties = new Dictionary<string, Bounds>();
+    [HideInInspector] [SerializeField] List<string> propertyNames = new List<string>();
+    [SerializeReference] [HideInInspector] [SerializeField] List<Bounds> propertyValues = new List<Bounds>();
+
+    public void OnBeforeSerialize()
+    {
+        propertyNames.Clear();
+        propertyValues.Clear();
+        foreach (var item in properties)
+        {
+            propertyNames.Add(item.Key);
+            propertyValues.Add(item.Value);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        Debug.Assert(propertyNames.Count == propertyValues.Count);
+        properties.Clear();
+        for (int i = 0; i < propertyNames.Count; i++)
+            properties[propertyNames[i]] = propertyValues[i];
+        propertyNames.Clear();
+        propertyValues.Clear();
     }
 }
